9053183984e0ece81ad167c350f4c0626903cec3
ra23th <ra23th@gmail.com>
2017-07-08 10:27:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 		$text = $event['message']['text'];
 		$type = $event['type'];
 		
-		$url = "http://110.76.155.50/jrsura/apiline.php?type=$type&userId=$userId&groupId=$groupId&roomId=$roomId&replyToken=$replyToken&text=$text";
+		$url = "http://xxx.xxx.xxx.xxx/jrsura/apiline.php?type=$type&userId=$userId&groupId=$groupId&roomId=$roomId&replyToken=$replyToken&text=$text";
 		$file = file_get_contents($url);
 
 		// Reply only when message sent is in 'text' format
